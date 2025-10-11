feec333c449d091e7c7f56146e001e365ef8d195
Eslam Linux <linuxeslam@gmail.com>
2025-10-11 22:44:38 +0300

    Update YouTubeDownloadDialog.cpp

--- src/UI/YouTubeDownloadDialog.cpp
+++ src/UI/YouTubeDownloadDialog.cpp
@@ -38,3 +38,42 @@ void YouTubeDownloadDialog::CreateUI() {
     m_savePathCtrl = new wxTextCtrl(this, wxID_ANY, wxGetHomeDir() + wxFileName::GetPathSeparator() + "Downloads");
     m_browseButton = new wxButton(this, wxID_ANY, "تصفح...");
     
+
+    wxStdDialogButtonSizer* buttonSizer = new wxStdDialogButtonSizer();
+    wxButton* okButton = new wxButton(this, wxID_OK, "تنزيل");
+    wxButton* cancelButton = new wxButton(this, wxID_CANCEL, "إلغاء");
+    buttonSizer->AddButton(okButton);
+    buttonSizer->AddButton(cancelButton);
+    buttonSizer->Realize();
+    
+    // إضافة عناصر واجهة المستخدم إلى الحاويات
+    wxBoxSizer* savePathSizer = new wxBoxSizer(wxHORIZONTAL);
+    savePathSizer->Add(m_savePathCtrl, 1, wxEXPAND | wxRIGHT, 5);
+    savePathSizer->Add(m_browseButton, 0, wxALIGN_CENTER_VERTICAL);
+    
+    mainSizer->Add(titleLabel, 0, wxEXPAND | wxALL, 5);
+    mainSizer->Add(m_titleCtrl, 0, wxEXPAND | wxLEFT | wxRIGHT | wxBOTTOM, 5);
+    mainSizer->Add(formatLabel, 0, wxEXPAND | wxALL, 5);
+    mainSizer->Add(m_formatChoice, 0, wxEXPAND | wxLEFT | wxRIGHT | wxBOTTOM, 5);
+    mainSizer->Add(savePathLabel, 0, wxEXPAND | wxALL, 5);
+    mainSizer->Add(savePathSizer, 0, wxEXPAND | wxLEFT | wxRIGHT | wxBOTTOM, 5);
+    mainSizer->Add(buttonSizer, 0, wxEXPAND | wxALL, 10);
+    
+    // تعيين الحاوية الرئيسية
+    SetSizer(mainSizer);
+    
+    // ربط الأحداث
+    m_browseButton->Bind(wxEVT_BUTTON, &YouTubeDownloadDialog::OnBrowse, this);
+    okButton->Bind(wxEVT_BUTTON, &YouTubeDownloadDialog::OnOK, this);
+    cancelButton->Bind(wxEVT_BUTTON, &YouTubeDownloadDialog::OnCancel, this);
+    
+    // تعيين القيم الافتراضية
+    m_titleCtrl->SetValue("YouTube Video");
+    
+    // تحميل معلومات الفيديو
+    m_downloader->GetVideoInfo(m_url, [this](const wxString& title) {
+        if (!title.IsEmpty()) {
+            m_titleCtrl->SetValue(title);
+        }
+    });
+}
