bb6baee6c818faaa91e878fcb43d15bbbe52f966
Eslam Linux <linuxeslam@gmail.com>
2025-10-11 22:54:09 +0300

    Update SpeedLimitDialog.cpp

--- src/UI/SpeedLimitDialog.cpp
+++ src/UI/SpeedLimitDialog.cpp
@@ -41,4 +41,53 @@ void SpeedLimitDialog::CreateUI()
   limitSizer->Add(m_limitCtrl, 0, wxALIGN_CENTER_VERTICAL | wxRIGHT, 5);
   limitSizer->Add(new wxStaticText(this, wxID_ANY, "KB/s"), 0, wxALIGN_CENTER_VERTICAL);
   mainSizer->Add(limitSizer, 0, wxEXPAND | wxLEFT | wxRIGHT | wxBOTTOM, 10);
+  
+  // Add buttons
+  wxBoxSizer* buttonSizer = new wxBoxSizer(wxHORIZONTAL);
+  buttonSizer->Add(new wxButton(this, wxID_OK, "OK"), 0, wxRIGHT, 5);
+  buttonSizer->Add(new wxButton(this, wxID_CANCEL, "Cancel"), 0);
+  mainSizer->Add(buttonSizer, 0, wxALIGN_RIGHT | wxALL, 10);
+  
+  // Set sizer
+  SetSizer(mainSizer);
+  
+  // Connect events
+  m_unlimitedRadio->Bind(wxEVT_RADIOBUTTON, &SpeedLimitDialog::OnRadioButton, this);
+  m_limitedRadio->Bind(wxEVT_RADIOBUTTON, &SpeedLimitDialog::OnRadioButton, this);
+  Bind(wxEVT_BUTTON, &SpeedLimitDialog::OnOK, this, wxID_OK);
+  
+  // Update UI
+  UpdateUI();
+}
 
+// Event handlers
+void SpeedLimitDialog::OnRadioButton(wxCommandEvent& event)
+{
+  UpdateUI();
+}
+
+void SpeedLimitDialog::OnOK(wxCommandEvent& event)
+{
+  // Update speed limit
+  if (m_unlimitedRadio->GetValue()) {
+      m_speedLimit = 0;
+  } else {
+      m_speedLimit = m_limitCtrl->GetValue();
+  }
+  
+  // Close dialog
+  EndModal(wxID_OK);
+}
+
+// Update UI
+void SpeedLimitDialog::UpdateUI()
+{
+  // Enable/disable limit control
+  m_limitCtrl->Enable(m_limitedRadio->GetValue());
+}
+
+// Get speed limit
+long SpeedLimitDialog::GetSpeedLimit() const
+{
+  return m_speedLimit;
+}
