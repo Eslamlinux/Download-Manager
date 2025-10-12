bea5c2eee10b01e75e79bd7dd1a35d480a616fd3
Eslam Linux <linuxeslam@gmail.com>
2025-10-12 14:11:44 +0300

    Update FileUtils.cpp

--- src/Utils/FileUtils.cpp
+++ src/Utils/FileUtils.cpp
@@ -27,3 +27,40 @@ bool FileExists(const wxString& path) {
     return wxFileExists(path);
 }
 
+// دالة لحذف الملف
+bool DeleteFile(const wxString& path) {
+    if (wxFileExists(path)) {
+        return wxRemoveFile(path);
+    }
+    
+    return false;
+}
+
+// دالة للحصول على امتداد الملف
+wxString GetFileExtension(const wxString& path) {
+    wxFileName fileName(path);
+    return fileName.GetExt();
+}
+
+// دالة للحصول على اسم الملف بدون المسار
+wxString GetFileName(const wxString& path) {
+    wxFileName fileName(path);
+    return fileName.GetFullName();
+}
+
+// دالة للحصول على اسم الملف بدون الامتداد
+wxString GetFileNameWithoutExtension(const wxString& path) {
+    wxFileName fileName(path);
+    return fileName.GetName();
+}
+
+// دالة للحصول على المسار بدون اسم الملف
+wxString GetFilePath(const wxString& path) {
+    wxFileName fileName(path);
+    return fileName.GetPath();
+}
+
+// دالة لإنشاء مسار كامل
+wxString CombinePath(const wxString& path, const wxString& fileName) {
+    return path + wxFileName::GetPathSeparator() + fileName;
+}
